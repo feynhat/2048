ab97dbe7471da7026c069ecefe1d8ab051b8d24b
Kumar Sannidhya Shukla <feynhat@gmail.com>
2016-11-18 00:53:19 +0530

    remove debug.js

--- debug.js
+++
@@ -1,9 +0,0 @@
-function pg(x) {
-    for (y in x.cells) {
-        s = y + ": ";
-        for (z in x.cells[y]) {
-            s += x.cells[y][z].value + " ";
-        }
-        console.log(s);
-    }
-}
